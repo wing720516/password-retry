6ebcdf083407b5b83a8456662daad6d357ddac61
Allen Wu <wing650503@gmail.com>
2021-02-05 16:48:08 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,14 @@
+password = 'a123456'
+i = 3
+
+while True:
+	number = input('請輸入密碼:')
+	if number == password:
+		print('登入成功!')
+		break
+	else:
+		i=i-1
+		print('密碼錯誤!還有',i,'次機會')
+		if i == 0:
+			break
+
